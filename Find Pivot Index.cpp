99080cd4a9bd9ad39219cd63bf6e641e3d197c54
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-08-14 22:57:43 -0300

    Create Find Pivot Index.cpp

---
+++ Find Pivot Index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        vector<int> sum_left = nums, sum_right = nums;
+        for (int i = 1; i < sum_left.size(); ++ i) {
+            sum_left[i] += sum_left[i - 1];
+        }
+        for (int i = sum_right.size() - 2; i >= 0; -- i) {
+            sum_right[i] += sum_right[i + 1];
+        }
+        for (int i = 0; i < nums.size(); ++ i) {
+            if (sum_left[i] == sum_right[i]) {
+                return i;
+            }
+        }
+        return -1;            
+    }
+};
